d37737b1c88a2f1da0b0f387e512d9e8f100d424
dshrskk <171007136+dshrskk@users.noreply.github.com>
2025-02-27 18:41:44 +0300

    Add main.c file

---
+++ main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define MAX_INPUT 1024
+
+int evaluate_expression(const char *expr) {
+    int stack[MAX_INPUT];
+    int top = -1;
+    int num = 0;
+    int i = 0;
+
+    while (expr[i] != '\0') {
+        if (isdigit(expr[i])) {
+            num = num * 10 + (expr[i] - '0');
+        } else if (expr[i] == '+'  expr[i] == '-'  expr[i] == '*' || expr[i] == '/') {
+            if (top >= 0) {
+                int b = stack[top--];
+                int a = stack[top--];
+
+                switch (expr[i]) {
+                    case '+':
+                        stack[++top] = a + b;
+                        break;
+                    case '-':
+                        stack[++top] = a - b;
+                        break;
+                    case '*':
+                        stack[++top] = a * b;
+                        break;
+                    case '/':
+                        stack[++top] = a / b;
+                        break;
+                }
+            } else {
+                stack[++top] = num;
+                num = 0;
+            }
+        } else if (expr[i] == '(') {
+            stack[++top] = num;
+            num = 0;
+        } else if (expr[i] == ')') {
+            stack[++top] = num;
+            num = 0;
+        }
+
+        i++;
+    }
+
+    return stack[top];
+}
+
+int main() {
+    char input[MAX_INPUT];
+    fgets(input, MAX_INPUT, stdin);
+
+    int result = evaluate_expression(input);
+    printf("%d\n", result);
+
+    return 0;
+}
