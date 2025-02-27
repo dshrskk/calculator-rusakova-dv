3b5463cbd8326ed56cedf5eb315e6dc01830fd1b
dshrskk <171007136+dshrskk@users.noreply.github.com>
2025-02-27 20:47:17 +0300

    Update main.c with corrected code2

--- main.c
+++ main.c
@@ -1,56 +1,105 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#define MAX_INPUT 1024
+#include <stdio.h>  
+#include <ctype.h>  
+#include <stdlib.h> 
 
-int evaluate_expression(const char *expr) {
-    int stack[MAX_INPUT];
-    int top = -1;
-    int num = 0;
-    int i = 0;
 
-    while (expr[i] != '\0') {
-        if (isdigit(expr[i])) {
-            num = num * 10 + (expr[i] - '0');
-        } else if (expr[i] == '+' || expr[i] == '-' || expr[i] == '*' || expr[i] == '/') {
-            if (top >= 1) { // Убедитесь, что стек содержит хотя бы два элемента
-                int b = stack[top--];
-                int a = stack[top--];
-                switch (expr[i]) {
-                    case '+':
-                        stack[++top] = a + b;
-                        break;
-                    case '-':
-                        stack[++top] = a - b;
-                        break;
-                    case '*':
-                        stack[++top] = a * b;
-                        break;
-                    case '/':
-                        stack[++top] = a / b;
-                        break;
-                }
-            } else {
-                stack[++top] = num;
-                num = 0;
-            }
-        } else if (expr[i] == '(') {
-            stack[++top] = num;
-            num = 0;
-        } else if (expr[i] == ')') {
-            stack[++top] = num;
-            num = 0;
+char ch;
+
+void get_next_char() {
+    ch = getchar(); 
+}
+void consume_whitespace() {
+    while (isspace(ch)) { 
+        get_next_char();  
+    }
+}
+
+int parse_expression() {
+    int value = parse_term(); 
+
+    while (ch == '+' || ch == '-') {
+        char op = ch;          
+        get_next_char();       
+        consume_whitespace();  
+
+        if (op == '+') {      
+            value += parse_term();
+        } else if (op == '-') { 
+            value -= parse_term();
         }
-        i++;
     }
 
-    return stack[top];
+    return value; 
+}
+
+int parse_term() {
+    int value = parse_factor(); 
+
+    while (ch == '*' || ch == '/') {
+        char op = ch;           
+        get_next_char();        
+        consume_whitespace();   
+
+        if (op == '*') {        // 
+            value *= parse_factor();
+        } else if (op == '/') { 
+            int divisor = parse_factor(); 
+            if (divisor == 0) {         
+                fprintf(stderr, "Ошибка: Деление на ноль\n");
+                exit(EXIT_FAILURE);      
+            }
+            value /= divisor;            
+        }
+    }
+
+    return value; 
+}
+
+(F -> число | (E))
+int parse_factor() {
+    consume_whitespace(); 
+
+    if (ch == '(') { 
+        get_next_char();              
+        int value = parse_expression(); 
+        consume_whitespace();        
+
+        if (ch != ')') { 
+            fprintf(stderr, "Ошибка: Ожидается ')'\n");
+            exit(EXIT_FAILURE); 
+        }
+
+        get_next_char(); 
+        return value;   
+    } else { 
+        int value = 0;
+
+        if (!isdigit(ch)) {
+            fprintf(stderr, "Ошибка: Ожидается число\n");
+            exit(EXIT_FAILURE); 
+        }
+
+        while (isdigit(ch)) {
+            value = value * 10 + (ch - '0'); 
+            get_next_char();                
+        }
+
+        return value; 
+    }
 }
 
 int main() {
-    char input[MAX_INPUT];
-    fgets(input, MAX_INPUT, stdin);
-    int result = evaluate_expression(input);
+    get_next_char(); 
+    int result = parse_expression();
+
+    consume_whitespace();
+
+    // Проверяем, что достигнут конец ввода
+    if (!feof(stdin)) {
+        fprintf(stderr, "Ошибка: Неожиданные символы после выражения\n");
+        return EXIT_FAILURE; 
+    }
     printf("%d\n", result);
-    return 0;
+
+    return 0; 
 }
